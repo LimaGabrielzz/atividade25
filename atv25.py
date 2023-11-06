c8aa005eff561ced23099760cb9e02ce0d7e8477
LimaGabrielzz <gabriel.lima318@aluno.ce.gov.br>
2023-11-06 10:36:47 -0300

    first commit

---
+++ atv25.py
@@ -0,0 +1,10 @@
+# Exercício Python 25: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas 
+# daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o. 
+
+numero = []
+for i in range(0,6):
+    n = int(input("Digite um número: "))
+    if n % 2 == 0:
+        numero.append(n)
+soma = sum(numero)
+print(soma)
